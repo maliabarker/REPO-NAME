6ba987b556b1c51431e682a45848245b3e4b112a
maliabarker <89502882+maliabarker@users.noreply.github.com>
2022-06-22 11:21:56 -1000

    Implemented full text search

--- models/pet.js
+++ models/pet.js
@@ -27,4 +27,10 @@ const PetSchema = new Schema({
 
 PetSchema.plugin(mongoosePaginate);
 
+// with weights
+PetSchema.index(
+  { name: 'text', species: 'text', favoriteFood: 'text', description: 'text' }, 
+  {name: 'My text index', weights: {name: 10, species: 4, favoriteFood: 2, description: 1}}
+);
+
 module.exports = mongoose.model('Pet', PetSchema);
